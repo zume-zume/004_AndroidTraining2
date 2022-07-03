16ef7fc49b55a42b9d5c3ccf4a1d2e45a7beb585
zume-zume <88725806+zume-zume@users.noreply.github.com>
2022-07-03 11:51:28 +0900

    【リストビュー】クリックイベントでトーストを表示させる対応

--- app/src/main/java/com/example/androidtraining2/MainActivity.java
+++ app/src/main/java/com/example/androidtraining2/MainActivity.java
@@ -1,12 +1,36 @@
 package com.example.androidtraining2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.LauncherActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ListViewオブジェクトを取得
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // ListViewをリスナに設定
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    // リストがタップされた時の処理が記述されたメンバクラス
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた時のアイスの名前
+            String item = (String) parent.getItemAtPosition(position);
+            // トーストで表示する文字列を生成
+            String show = "あなたが選んだアイス";
+            // トーストの表示
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
     }
 }
